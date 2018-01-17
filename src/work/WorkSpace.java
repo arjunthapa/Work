f2da39d4d398294f8130017c4390a9165be7dc3b
Arjun Thapa <bad.boy7.bb26@gmail.com>
2018-01-17 16:14:50 +0545

    Referencing ListManager and some codes changed.

--- src/work/WorkSpace.java
+++ src/work/WorkSpace.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 /*
@@ -23,10 +24,11 @@ public class WorkSpace extends JPanel {
     private Dimension dim;
     private JPanel WorkSpacePane;
     private JButton btn;
+    private ListManager lm;
     
-    
-    public WorkSpace(Dimension dim)
+    public WorkSpace(Dimension dim,ListManager lm)
     {
+        this.lm = lm;
         this.dim=dim;
         this.setPreferredSize(dim);
         WorkSpacePane = new JPanel();
@@ -45,9 +47,15 @@ public class WorkSpace extends JPanel {
          btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                PlayScreen ps;
-                ps = new PlayScreen();
-                ps.newFrame();
+                createScreen();
+            }
+            public void createScreen()
+            {
+                JFrame jf = new JFrame();
+                jf.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                jf.setUndecorated(true);
+                jf.add(new PlayScreen(lm.getLyricsManager()));
+                jf.setVisible(true);
             }
         });
     }
