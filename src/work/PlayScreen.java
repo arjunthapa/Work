e36bb1b5f98dd35488f33b852c4e6e5f9e76a4a4
Arjun Thapa <bad.boy7.bb26@gmail.com>
2018-01-18 17:35:25 +0545

    Playscreen Working properly but need to arrange the words and chords.

--- src/work/PlayScreen.java
+++ src/work/PlayScreen.java
@@ -6,6 +6,7 @@
 package work;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import javax.swing.JPanel;
@@ -15,38 +16,48 @@ import javax.swing.JPanel;
  * @author SHAUGAN
  */
 public class PlayScreen extends JPanel {
-    
-    
+
     //LyricsManager reference which will be passed by the ListManager to Playscreen.
     LyricsManager lm;
     LyricsLine cl;
-    
+
     /**
      * Play screen will show the lyrics line on screen
+     *
+     * @param lm
      */
-    public PlayScreen(LyricsManager lm)
-    {
+    public PlayScreen(LyricsManager lm) {
+        super.setBackground(Color.red);
         this.lm = lm;
-        
+        System.out.println("No of lines found in Playscreen: " + lm.getLyrics().size());
+        if (lm.getLyrics().size() > 0) {
+            cl = lm.getLyrics().get(0);
+            System.out.println(cl.getLine());
+        }
     }
+
     @Override
-    public void paint(Graphics g)
-    {
-        cl = lm.getLyrics().get(0);
-        ArrayList<LyricsWords> lw = cl.getLyricsWords();
-        g.setColor(Color.red);
+    public void paint(Graphics g) {
+          g.setFont(new Font("TimesRoman", Font.PLAIN, 40));
+      
+         ArrayList<LyricsWords> lw = cl.getLyricsWords();
+//         g.setColor(Color.RED);
+////        g.drawString(TOOL_TIP_TEXT_KEY, WIDTH, WIDTH);
+            int x=100;
         for (LyricsWords lyricsWords : lw) {
-            
+            g.setColor(lyricsWords.getColor());
+            x=x+(lyricsWords.getWords().length()*30);
+            g.drawString(lyricsWords.getChord(), x, 350);
+            g.drawString(lyricsWords.getWords(), x, 400);
         }
         
-        
-        g.drawString("", 300, 400);
+      
     }
-    public boolean nextLine()
-    {
+
+    public boolean nextLine() {
         //Here goes the code to remove the first line and add the last line.
         this.repaint();
         return false;
     }
-    
+
 }
