47c1f97fce750ffc9bb3a099aa45e30783141c68
Arjun Thapa <bad.boy7.bb26@gmail.com>
2018-01-19 06:31:03 +0545

    Code working.

--- test/LyricsManagerTest.java
+++ test/LyricsManagerTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import org.junit.After;
@@ -11,7 +5,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import work.LyricsLine;
 import work.LyricsManager;
 import work.LyricsWords;
